f977eed6146d1f9402a55dab14955edb2bf52f96
Takaaki Araki <arakisoccer@gmail.com>
2020-12-13 04:00:16 +0000

    20201213

--- W_league/config/routes.rb
+++ W_league/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # un aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
+  get '/home', to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
